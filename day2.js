f3e2cf9ae3f82258c6611f6bbe2fdcd1f06fdd69
Biderohi <biderohimohapatra@gmail.com>
2024-01-25 00:06:33 +0530

    new js file add

--- day2.js
+++ day2.js
@@ -129,3 +129,31 @@ form.addEventListener("submit", async (e) => {
     .querySelector("input")
     .addEventListener("click", () => location.reload());
 });
+AudioDestinationNode
+document.getElementById("btn").addEventListener("click", joke);
+
+async function joke() {
+  let config = {
+    headers: {
+      Accept: "application/json",
+    },
+  };
+
+  let a = await fetch("https://icanhazdadjoke.com/", config);
+  let b = await a.json();
+  document.getElementById("content").innerHTML = b.joke;
+}
+document.getElementById("btn").addEventListener("click", joke);
+
+async function joke() {
+  let config = {
+    headers: {
+      Accept: "application/json",
+    },
+  };
+
+  let a = await fetch("https://icanhazdadjoke.com/", config);
+  let b = await a.json();
+  document.getElementById("content").innerHTML = b.joke;
+}
+
