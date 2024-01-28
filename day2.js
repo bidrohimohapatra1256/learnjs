475d4b96265e21e06c6c72d7b076e0edc1e6175d
Biderohi <biderohimohapatra@gmail.com>
2024-01-28 20:40:45 +0530

    commit

--- day2.js
+++ day2.js
@@ -164,5 +164,19 @@ menuBtn.addEventListener("click", () => {
   sidebar.classList.toggle("active");
   content.classList.toggle("active");
 });
+const btn = document.querySelector(".btn");
+const closeIcon = document.querySelector(".close-icon");
+const trailerContainer = document.querySelector(".trailer-container");
+const video = document.querySelector("video");
+
+btn.addEventListener("click", () => {
+  trailerContainer.classList.remove("active");
+});
+
+closeIcon.addEventListener("click", () => {
+  trailerContainer.classList.add("active");
+  video.pause();
+  video.currentTime = 0;
+});
 
 
