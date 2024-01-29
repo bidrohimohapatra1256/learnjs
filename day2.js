6ff1c03d10f2ddc46fb5ef13c818aff710440c06
Biderohi <biderohimohapatra@gmail.com>
2024-01-29 20:40:00 +0530

    commit

--- day2.js
+++ day2.js
@@ -177,6 +177,15 @@ closeIcon.addEventListener("click", () => {
   trailerContainer.classList.add("active");
   video.pause();
   video.currentTime = 0;
+});const password = document.getElementById("password");
+const bg = document.querySelector(".background-image");
+
+password.addEventListener("input", (e) => {
+  const input = e.target.value;
+  const length = input.length;
+  const blurness = 20 - length * 2;
+  bg.style.filter = `blur(${blurness}px)`;
 });
+
 
 
