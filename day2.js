5b2897663109a2a0974ce382257109cbc0b37120
Biderohi <biderohimohapatra@gmail.com>
2024-02-06 21:27:10 +0530

    commit

--- day2.js
+++ day2.js
@@ -206,7 +206,29 @@ textInput.addEventListener("input", () => {
 
  
 });
+day2 day2/index.js day2/index.js/index.html day2/index.js/style.css index.js project project/index.html project/index.html/app.js project/index.html/index.html project/index.html/style.css project4 project4/dad5 da5.js day2.js fast.js index.html style.css
+const itemHeaders = document.querySelectorAll(".accordion-item-header");
 
+itemHeaders.forEach((accordion) => {
+  accordion.addEventListener("click", collapseAccordions);
+
+  function collapseAccordions() {
+    const activeAccordion = document.querySelector(".active");
+    if (activeAccordion && activeAccordion !== accordion) {
+      activeAccordion.classList.toggle("active");
+      activeAccordion.nextElementSibling.style.maxHeight = 0;
+    }
+
+    accordion.classList.toggle("active");
+    const accordionItemBody = accordion.nextElementSibling;
+
+    if (accordion.classList.contains("active")) {
+      accordionItemBody.style.maxHeight = accordionItemBody.scrollHeight + "px";
+    } else {
+      accordionItemBody.style.maxHeight = 0;
+    }
+  }
+});
 
 
 
