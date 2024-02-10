efa36719f73680440cc3e6a0f4c1f0a46def0cef
Biderohi <biderohimohapatra@gmail.com>
2024-02-10 21:22:28 +0530

    commit

--- fast.js
+++ fast.js
@@ -17,3 +17,14 @@ const cars = ["Saab", "Volvo", "BMW"];
 
 // Date object:
 const date = new Date("2022-03-25");
+const p = document.getElementById("p");
+const searchBtn = document.getElementById("searchBtn");
+
+searchBtn.addEventListener("click", () => {
+  let input = document.getElementById("input").value;
+
+  if (input !== "") {
+    let regExp = new RegExp(input, "gi");
+    p.innerHTML = p.textContent.replace(regExp, "<mark>$&</mark>");
+  }
+});
