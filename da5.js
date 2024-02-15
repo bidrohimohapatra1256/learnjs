a35d06d558b69eb74a2c2908f45277654a5486a8
Biderohi <biderohimohapatra@gmail.com>
2024-02-15 07:32:01 +0530

    commit

--- da5.js
+++ da5.js
@@ -50,12 +50,13 @@ btn.addEventListener("click", generateGradient);
 // We are still using Rollup for production builds because it generates
 // smaller files and provides better tree-shaking.
 
-import esbuild from 'esbuild'
-import { dirname, relative, resolve } from 'node:path'
-import { fileURLToPath } from 'node:url'
-import { createRequire } from 'node:module'
-import minimist from 'minimist'
-import { polyfillNode } from 'esbuild-plugin-polyfill-node'
+import esbuild from 'esbuild';
+import { polyfillNode } from 'esbuild-plugin-polyfill-node';
+import minimist from 'minimist';
+import { createRequire } from 'node:module';
+import { dirname, relative, resolve } from 'node:path';
+import { fileURLToPath } from 'node:url';
+import type { MockInstance } from 'vitest';
 
 const require = createRequire(import.meta.url)
 const __dirname = dirname(fileURLToPath(import.meta.url))
@@ -177,4 +178,99 @@ for (const target of targets) {
     })
     .then(ctx => ctx.watch())
 }
+
+vi.stubGlobal('MathMLElement', class MathMLElement {})
+
+expect.extend({
+  toHaveBeenWarned(received: string) {
+    const passed = warn.mock.calls.some(args => args[0].includes(received))
+    if (passed) {
+      asserted.add(received)
+      return {
+        pass: true,
+        message: () => `expected "${received}" not to have been warned.`,
+      }
+    } else {
+      const msgs = warn.mock.calls.map(args => args[0]).join('\n - ')
+      return {
+        pass: false,
+        message: () =>
+          `expected "${received}" to have been warned` +
+          (msgs.length
+            ? `.\n\nActual messages:\n\n - ${msgs}`
+            : ` but no warning was recorded.`),
+      }
+    }
+  },
+
+  toHaveBeenWarnedLast(received: string) {
+    const passed =
+      warn.mock.calls[warn.mock.calls.length - 1][0].includes(received)
+    if (passed) {
+      asserted.add(received)
+      return {
+        pass: true,
+        message: () => `expected "${received}" not to have been warned last.`,
+      }
+    } else {
+      const msgs = warn.mock.calls.map(args => args[0]).join('\n - ')
+      return {
+        pass: false,
+        message: () =>
+          `expected "${received}" to have been warned last.\n\nActual messages:\n\n - ${msgs}`,
+      }
+    }
+  },
+
+  toHaveBeenWarnedTimes(received: string, n: number) {
+    let found = 0
+    warn.mock.calls.forEach(args => {
+      if (args[0].includes(received)) {
+        found++
+      }
+    })
+
+    if (found === n) {
+      asserted.add(received)
+      return {
+        pass: true,
+        message: () => `expected "${received}" to have been warned ${n} times.`,
+      }
+    } else {
+      return {
+        pass: false,
+        message: () =>
+          `expected "${received}" to have been warned ${n} times but got ${found}.`,
+      }
+    }
+  },
+})
+
+let warn: MockInstance
+const asserted: Set<string> = new Set()
+
+beforeEach(() => {
+  asserted.clear()
+  warn = vi.spyOn(console, 'warn')
+  warn.mockImplementation(() => {})
+})
+
+afterEach(() => {
+  const assertedArray = Array.from(asserted)
+  const nonAssertedWarnings = warn.mock.calls
+    .map(args => args[0])
+    .filter(received => {
+      return !assertedArray.some(assertedMsg => {
+        return received.includes(assertedMsg)
+      })
+    })
+  warn.mockRestore()
+  if (nonAssertedWarnings.length) {
+    throw new Error(
+      `test case threw unexpected warnings:\n - ${nonAssertedWarnings.join(
+        '\n - ',
+      )}`,
+    )
+  }
+})
 
